48d8e5a05b597d0211edf9235e25b3e0665b8455
geneningz <geneningz@outlook.com>
2023-05-01 20:23:21 +0800

    feature(ui): 构建文件上传功能；feature(ui): 添加黑夜模式切换

--- src/components/ChatWindow.vue
+++ src/components/ChatWindow.vue
@@ -3,11 +3,11 @@
         <div class="h-[5%] flex items-center">
             <span class="text-size-[16px]"> {{ props.activeDoc.name }} </span>
         </div>
-        <div class="h-[85%] bg-gray bg-opacity-5 rounded-[4px] px-[8px] flex flex-col gap-[10px]">
+        <div class="h-[85%] bg-gray dark:bg-white dark:bg-opacity-15 bg-opacity-5 rounded-[4px] px-[8px] flex flex-col gap-[10px]">
             <div class="flex flex-row gap-[5px]">
-                <iconpark-icon class="w-[32px] h-[32px] rounded-full p-[8px] bg-white text-black text-size-[24px]" name="robot" />
-                <div class="bg-blue bg-opacity-25 rounded-r-[8px] rounded-lb-[8px] px-[16px] py-[4px] flex items-center">
-                    <span class="text-size-[16px] text-gray"> {{ props.activeDoc.name }} </span>
+                <iconpark-icon class="w-[32px] h-[32px] rounded-full p-[8px] bg-white dark:bg-blue dark:bg-opacity-75 text-black dark:text-white text-size-[24px]" name="robot" />
+                <div class="bg-blue dark:bg-white dark:bg-opacity-50 bg-opacity-25 rounded-r-[8px] rounded-lb-[8px] px-[16px] py-[4px] flex items-center">
+                    <span class="text-size-[16px] text-gray dark:text-white"> {{ props.activeDoc.name }} </span>
                 </div>
             </div>
             <div
@@ -16,7 +16,7 @@
                 :key="index" 
                 class="flex flex-row gap-[5px]"
             >
-                <iconpark-icon class="w-[32px] h-[32px] rounded-full p-[8px] bg-white text-blue text-size-[24px]" name="user-business" />
+                <iconpark-icon class="w-[32px] h-[32px] rounded-full p-[8px] bg-white  dark:bg-blue dark:bg-opacity-75 text-blue text-size-[24px]" name="user-business" />
                 <div
                     class="bg-white bg-opacity-50 rounded-r-[8px] rounded-lb-[8px] px-[16px] py-[4px] flex items-center">
                     <span class="text-size-[16px] text-gray"> {{ msg }} </span>
@@ -24,7 +24,7 @@
             </div>
         </div>
         <div class="h-[10%] flex flex-row justify-center items-center">
-            <div class="h-[48px] w-[100%] bg-white bg-opacity-80 rounded-[8px] border-gray border flex flex-row justify-center items-center px-[4px]">
+            <div class="h-[48px] w-[100%] bg-white dark:bg-blue dark:bg-opacity-50 bg-opacity-80 rounded-[8px] border-gray border flex flex-row justify-center items-center px-[4px]">
                 <input 
                     v-model="content" 
                     type="text" 
@@ -32,7 +32,7 @@
                     @keydown.enter="sendMsg"
                 >
                 <iconpark-icon 
-                    class="w-[32px] h-[32px] text-size-[24px] text-gray rounded-full p-[4px] bg-blue bg-opacity-25 cursor-pointer" 
+                    class="w-[32px] h-[32px] text-size-[24px] text-gray dark:text-white rounded-full p-[4px] bg-blue  dark:bg-gray dark:bg-opacity-75 bg-opacity-25 cursor-pointer" 
                     name="send"
                     @click="sendMsg"
                 />
